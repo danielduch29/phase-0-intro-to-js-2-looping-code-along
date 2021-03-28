88476a7888f2c92ebb5779d3b4208e85eacded12
Daniel <Daniel.duchatellier@gmail.com>
2021-03-28 19:59:19 -0400

    having trouble with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(){
+    let arr = writeCards(0);
+    let gift = writeCards(1);
+    for(let i = 0;i < arr.length;i++){
+        console.log("Thank you, ${arr[i]}, for the wonderful surprise gift!");
+        debugger;
+    }
+    return arr;
+}
+
+function countDown(){
+    console.log(countDown);
+}
